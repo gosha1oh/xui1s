ab37189e914b2a27a6d742718706c6b5fb726181
gosha1oh <38810921+gosha1oh@users.noreply.github.com>
2018-12-18 21:17:40 +0300

    Add files via upload

---
+++ config1.php
@@ -0,0 +1,9 @@
+<?php
+///Jangan hapus tanpa kutip nya(")
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+///Ganti xxxxxxx dengan punya kalian
+///Capture dulu apk nya ambil yang di butuhkan
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBin-UsLqjr7-Lnc28o4ZtfqyykoK5hWXAiIKfkX2-k4udhNCvqqOys9uYl62jra6KZM3XjZSKo7-Ik2uRo6zWlGWLoQ";
+$meid = "c5722a4972ed6a9c";
+$os="android";
+$sign="9675d5188726861e4ea65231213c31cf60bc00bc9a93bc5c4d669ef29499d6fe";
